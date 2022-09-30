1c0ae2cc2a9ddfb5949bddf7858fa4ea59bafea7
gulten27 <ggdogann00@gmail.com>
2022-09-30 20:36:00 +0300

    commit

--- app/src/main/java/com/gultendogan/weightlyapp/ui/onboarding/OnBoardingFragment.kt
+++ app/src/main/java/com/gultendogan/weightlyapp/ui/onboarding/OnBoardingFragment.kt
@@ -8,6 +8,9 @@ import com.gultendogan.weightlyapp.R
 import com.gultendogan.weightlyapp.databinding.FragmentOnBoardingBinding
 import com.gultendogan.weightlyapp.uicomponents.CardRuler
 import com.gultendogan.weightlyapp.utils.viewBinding
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.flow.collect
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,12 +23,30 @@ class OnBoardingFragment : Fragment(R.layout.fragment_on_boarding) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews()
+        observe()
+    }
+
+    private fun observe() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.eventsFlow.collect { event ->
+                when (event) {
+                    OnBoardingViewModel.Event.NavigateToHome -> {
+                        findNavController().navigate(OnBoardingFragmentDirections.actionNavigateHome())
+                    }
+                }
+            }
+        }
     }
 
 
     private fun initViews() = with(binding) {
-        cardRulerCurrent.render(CardRuler(R.string.current_weight,R.string.enter_current_weight))
-        cardRulerGoal.render(CardRuler(R.string.goal_weight,R.string.enter_goal_weight))
+        cardRulerCurrent.render(CardRuler(R.string.current_weight, R.string.enter_current_weight))
+        cardRulerGoal.render(CardRuler(R.string.goal_weight, R.string.enter_goal_weight))
+        btnContinue.setOnClickListener {
+            val currentWeight: Float = cardRulerCurrent.value
+            val goalWeight: Float = cardRulerGoal.value
+            viewModel.save(currentWeight = currentWeight, goalWeight = goalWeight)
+        }
     }
 
 }
--- app/src/main/java/com/gultendogan/weightlyapp/utils/Constants.kt
+++ app/src/main/java/com/gultendogan/weightlyapp/utils/Constants.kt
@@ -2,6 +2,10 @@ package com.gultendogan.weightlyapp.utils
 
 object Constants {
 
-    const val KEY_SHOULD_SHOW_ON_BOARDING = "key_pref_on_boarding"
+    object Prefs{
+        const val KEY_GOAL_WEIGHT = "key_goal_weight"
+        const val KEY_SHOULD_SHOW_ON_BOARDING = "key_pref_on_boarding"
+
+    }
 
 }
